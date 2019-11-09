403f719956343d267c79301338ee1fa5d83283ec
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-11-09 15:32:08 +0530

    mocking data, models, and stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
+import "./location_detail.dart";
+import "./models/location.dart";
+import "./mocks/mock_location.dart";
 
-void main() => runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: Text("First App"),
-        ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                decoration: BoxDecoration(color: Colors.red),
-                child: Text("one"),
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.green),
-                child: Text("two"),
-              ),
-              Container(
-                decoration: BoxDecoration(color: Colors.blue),
-                child: Text("three"),
-              )
-            ]))));
+void main() {
+  final Location mockLocation = MockLocation.FetchAny();
+  return runApp(MaterialApp(home: LocationDetail(mockLocation)));
+}
